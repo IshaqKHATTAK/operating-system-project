a29b4278f5166fa956c7a73fab4340d977fa7d13
IshaqKHATTAK <118132901+IshaqKHATTAK@users.noreply.github.com>
2022-12-05 02:00:51 +0500

    milestone-2

--- test_trace.c
+++ test_trace.c
@@ -2,13 +2,19 @@
 #include "stat.h"
 #include "user.h"
 
-
-int main(void)
+int main(int argc,char *argv[])
 {
 
-printf(1,"this should return 999 and it reurn %d \n",trace());
+if(argc<2 || argc>2)
+{
+return -1;
+}
+else
+  {
+printf(1,"reutrn value of trace in side test_trace= %d \n",trace(argv[1]));
+    }
 
-exit();
+  exit();
 
 
 }
